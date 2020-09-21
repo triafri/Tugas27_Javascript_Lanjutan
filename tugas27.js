2be23ae16c3518683db7b448c7cb9406b41237b5
TRI AFRILIYANTI <71365606+triafri@users.noreply.github.com>
2020-09-22 00:18:24 +0700

    Add files via upload

---
+++ tugas27.js
@@ -0,0 +1,9 @@
+function joinArray() {
+	var arr1 = ["No","rumah saya",30,"A"]
+	var arr2 = ["Jalan pangeran Tirtayasa No", 70]
+
+	console.log(arr1.join(" "))
+	console.log(arr2.join(" "))
+}
+
+joinArray();
